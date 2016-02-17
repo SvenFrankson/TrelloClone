ee81d37164a20df1caf5adb4502d037323cc087e
Sven Frankson <sven.frankson@gmail.com>
2016-02-17 21:36:06 +0100

    Adding Room Controller.

--- public/app.js
+++ public/app.js
@@ -31,12 +31,71 @@ app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $u
 
 app.controller('HomeController', ['$scope', 'rooms', function ($scope, rooms) {
     "use strict";
-    rooms.rooms.push({name: "My test room"});
-    rooms.rooms.push({name: "My other room"});
-    rooms.rooms.push({name: "My third room"});
+    rooms.rooms = [];
+    rooms.rooms.push({
+        name: "My test room",
+        taskLists: [
+            {
+                name: "TaskList1",
+                tasks: [
+                    "Do the dishes",
+                    "Do the laundry"
+                ]
+            },
+            {
+                name: "TaskListMore",
+                tasks: [
+                    "Do more dishes",
+                    "Do more laundry"
+                ]
+            }
+        ],
+        users: ["Andrew", "Bryan", "Chuck"]
+    });
+    rooms.rooms.push({
+        name: "My other room",
+        taskLists: [
+            {
+                name: "TaskList2",
+                tasks: [
+                    "Do the carwash",
+                    "Do the laundry"
+                ]
+            },
+            {
+                name: "TaskListNext",
+                tasks: [
+                    "Do more dishes",
+                    "Do more carwash"
+                ]
+            }
+        ],
+        users: ["Danny", "Eliott", "Frank"]
+    });
+    rooms.rooms.push({
+        name: "My third room",
+        taskLists: [
+            {
+                name: "TaskList3",
+                tasks: [
+                    "Do the carwash",
+                    "Do the laundry"
+                ]
+            },
+            {
+                name: "TaskListOther",
+                tasks: [
+                    "Do nothing",
+                    "Do more carwash"
+                ]
+            }
+        ],
+        users: ["Andrew", "Chuck", "Eliott"]
+    });
     $scope.rooms = rooms.rooms;
 }]);
 
 app.controller('RoomController', ['$scope', '$stateParams', 'rooms', function ($scope, $stateParams, rooms) {
     "use strict";
+    $scope.room = rooms.rooms[$stateParams.id];
 }]);
